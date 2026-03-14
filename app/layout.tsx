5455b0a7ce8f9e2483c53a4f19fa74ecf51e54bc
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-14 14:15:15 +0100

    feat(layout): add page.module.css to layout and scroll-behavior: smooth on the html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope, Inter } from "next/font/google";
 import Header from '@/components/Header/Header';
 
+import css from '@/app/page.module.css';
 import "./globals.css";
 
 const manrope = Manrope({
@@ -43,11 +44,11 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-      <html lang="en">
+      <html lang="en" data-scroll-behavior="smooth">
         <body className={`${manrope.variable} ${inter.variable}`}>
           <Header />
           {children}
         </body>
       </html>
     );
-};
+}

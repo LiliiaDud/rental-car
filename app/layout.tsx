837db1e4a80a5ecec83e2e79ad8ecd61d0b5f2ba
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-15 23:06:28 +0100

    feat: configure global layout with TanStack Query and Toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from "next";
-import { Manrope, Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Manrope, Inter } from 'next/font/google';
 import Header from '@/components/Header/Header';
-
-import css from '@/app/page.module.css';
-import "./globals.css";
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import { Toaster } from 'react-hot-toast';
+import './globals.css';
 
 const manrope = Manrope({
-    subsets: ["latin"],
-    variable: "--font-manrope",
-    display: "swap",
+  subsets: ['latin'],
+  variable: '--font-manrope',
+  display: 'swap',
 });
 
 const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-inter",
-    display: "swap",
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
-    title: "RentalCar | Car Rental Service",
-    description: "Reliable and budget-friendly car rentals for any journey.",
-    icons: {
-      icon: '/favicon.ico',
-    },
+  title: 'RentalCar | Car Rental Service',
+  description: 'Reliable and budget-friendly car rentals for any journey.',
+  icons: {
+    icon: '/favicon.ico',
+  },
   openGraph: {
     title: 'Rental Car App',
     description: 'A platform for renting cars with ease and convenience',
@@ -39,16 +39,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-      <html lang="en" data-scroll-behavior="smooth">
-        <body className={`${manrope.variable} ${inter.variable}`}>
+  return (
+    <html lang="en" data-scroll-behavior="smooth">
+      <body className={`${manrope.variable} ${inter.variable}`}>
+        <TanStackProvider>
           <Header />
-          {children}
-        </body>
-      </html>
-    );
+          <main>{children}</main>
+          <Toaster position="top-right" />
+        </TanStackProvider>
+      </body>
+    </html>
+  );
 }

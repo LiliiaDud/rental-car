7054a5aef680ef915e8c0fb2689534a930ff432a
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-17 17:26:44 +0100

    feat(car-details): add dynamic car details page with server data fetching

--- app/catalog/[id]/page.tsx
+++ app/catalog/[id]/page.tsx
@@ -1,6 +1,19 @@
-const CarDetailsPage = () => {
+import CarDetails from '@/components/CarDetails/CarDetails';
+import { getCarById } from '@/lib/api/clientApi';
 
-    return < CarDetailsClient />;
-};
+interface CarDetailsProps {
+  params: Promise<{ id: string }>;
+}
+
+async function CarDetailsPage({ params }: CarDetailsProps) {
+  const { id } = await params;
+
+  const car = await getCarById(id);
+  return (
+    <div>
+      <CarDetails car={car} />
+    </div>
+  );
+}
 
 export default CarDetailsPage;

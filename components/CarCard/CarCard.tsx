3ee32586f5e8520ba09d322afea327bf2099e8a5
Liliia Dudnyk <91532622+LiliiaDud@users.noreply.github.com>
2026-03-19 15:00:38 +0100

    refactor(car-card): replace local favorite state with zustand store

--- components/CarCard/CarCard.tsx
+++ components/CarCard/CarCard.tsx
@@ -3,15 +3,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Car } from '@/types/car';
-import { useState } from 'react';
 import css from './CarCard.module.css';
+import { useCarsStore } from '@/lib/store/carsStore';
 
 interface Props {
   car: Car;
 }
 
 function CarCard({ car }: Props) {
-  const [isFavorite, setIsFavorite] = useState(false);
+  const { favorites, toggleFavorite } = useCarsStore();
+  const isFavorite = favorites.includes(String(car.id));
+
   const [, city, country] = car.address.split(', ');
 
   const mileage = car.mileage.toLocaleString('uk-UA');
@@ -27,7 +29,7 @@ function CarCard({ car }: Props) {
           height={268}
         />
         <button
-          onClick={() => setIsFavorite(prev => !prev)}
+          onClick={() => toggleFavorite(String(car.id))}
           className={css.favoriteBtn}
           type="button"
         >
